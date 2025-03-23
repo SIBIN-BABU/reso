ef7150aa5de5b18094f21005421d8ab561934b17
SIBIN-BABU <77160268+SIBIN-BABU@users.noreply.github.com>
2025-03-23 20:23:17 +0100

    Update url.py

--- payment/url.py
+++ payment/url.py
@@ -9,6 +9,7 @@ from  .import views
 urlpatterns = [
    path("",views.payment_sucess,name="payment_sucess"),
    path("payment_failed",views.payment_failed,name="payment_failed"),
+   path("chechout/",views.checkout,name="checkout"),
    path("billing_info/",views.billing_info,name="billing_info"),
    path("payment/",views.payment_info,name="payment"),
    path("shipped_item/",views.shipped_item,name="shipped_item"),
