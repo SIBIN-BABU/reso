c1a213da4799b84bc496599e9dce0e98e2a0e26f
SIBIN-BABU <babusibin5@gmail.com>
2025-03-25 13:46:05 +0100

    trending products

--- pro/views.py
+++ pro/views.py
@@ -128,7 +128,8 @@ def Register_user(request):
 
 def product(request,pk):
     product = Product.objects.get(id=pk)
-    pros = Product.objects.filter(category__name="Shoes,Accessories")
+    pros = Product.objects.filter(category__name__in=["Shoes", "Accessories"])
+
 
    
     
